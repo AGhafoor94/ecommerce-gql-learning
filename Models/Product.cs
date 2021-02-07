e1981bb5d0e01136c4f55c38976f34c35ae99a77
Adnan <adnan_ghafoor94@hotmail.com>
2021-02-07 15:59:10 +0000

    Created Product model and linking multiple products to a single category

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,16 +1,17 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace ecommerce_gql_learning.Models
 {
-    public class Category
+    public class Product
     {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        // Collection of products for a category
-        public ICollection<Product> Products { get; set; } = new List<Product>();
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
